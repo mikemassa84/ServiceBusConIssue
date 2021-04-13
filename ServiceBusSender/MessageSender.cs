8533d33c971c94d1488dd7324a590a1016487b31
Mike Massa <michael.massa@nm.org>
2021-04-13 16:16:01 -0500

    project to send messages

---
+++ ServiceBusSender/MessageSender.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Azure.ServiceBus;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceBusSender
+{
+    public static class MessageSender
+    {
+        private const string QueueName = "myqueue";
+
+        private static readonly string ConStr;
+        private static readonly IQueueClient Client;
+
+        static MessageSender()
+        {
+            ConStr = Environment.GetEnvironmentVariable("servicebus_con");
+            Client = new QueueClient(ConStr, QueueName);
+        }
+
+        public static async Task SendMessageAsync(string msg)
+        {
+            var message = new Message(Encoding.UTF8.GetBytes(msg))
+            {
+                Label = "Test",
+                MessageId = Guid.NewGuid().ToString()
+            };
+            Console.WriteLine($"Sending message: {msg}");
+
+            await Client.SendAsync(message).ConfigureAwait(false);
+        }
+
+        public static async Task Close()
+        {
+            await Client.CloseAsync().ConfigureAwait(false);
+        }
+    }
+}
---
+++ ServiceBusSender/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+
+namespace ServiceBusSender
+{
+    public static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            for (int i = 0; i < 25000; i++)
+            {
+                await Task.Run(() => MessageSender.SendMessageAsync($"Message {i} sent!").ConfigureAwait(false));
+            }
+
+            await MessageSender.Close().ConfigureAwait(false);
+        }
+    }
+}
